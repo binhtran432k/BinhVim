321c3392cc9f55d8239af6fe08e62c9173afcce2
Binh Duc Tran <binhtran432k@gmail.com>
2024-05-05 11:01:03 +0700

    feat(extras): add zig

---
+++ fnl/binhvim/extras/lang/zig.fnl
@@ -0,0 +1,4 @@
+[{1 :neovim/nvim-lspconfig :opts {:servers {:zls {}}}}
+ {1 :nvim-treesitter/nvim-treesitter
+  :opts {:binh_ensure_installed {:zig true}}}]
+
---
+++ lua/binhvim/extras/lang/zig.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/binhvim/extras/lang/zig.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"neovim/nvim-lspconfig", opts = {servers = {zls = {}}}}, {"nvim-treesitter/nvim-treesitter", opts = {binh_ensure_installed = {zig = true}}}}
