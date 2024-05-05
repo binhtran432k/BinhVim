edb3d3a7db59b158b08a8abc32c17c557ce11294
Binh Duc Tran <binhtran432k@gmail.com>
2024-05-05 11:00:50 +0700

    feat(extras): add tsserver

---
+++ fnl/binhvim/extras/lang/tsserver.fnl
@@ -0,0 +1,5 @@
+[;; add typescript to treesitter
+ {1 :nvim-treesitter/nvim-treesitter
+  :opts {:binh_ensure_installed {:typescript true :tsx true}}}
+ {1 :pmizio/typescript-tools.nvim :opts {}}]
+
---
+++ lua/binhvim/extras/lang/tsserver.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/binhvim/extras/lang/tsserver.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"nvim-treesitter/nvim-treesitter", opts = {binh_ensure_installed = {typescript = true, tsx = true}}}, {"pmizio/typescript-tools.nvim", opts = {}}}
