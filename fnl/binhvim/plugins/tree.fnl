aaa3d499635bb7a2ca06b64bc1e333b2482869a9
Binh Duc Tran <binhtran432k@gmail.com>
2024-05-11 01:04:04 +0700

    feat(tree): add useful keybindings

--- fnl/binhvim/plugins/tree.fnl
+++ fnl/binhvim/plugins/tree.fnl
@@ -4,7 +4,14 @@
  :keys [{1 :<leader>e 2 :<cmd>NvimTreeToggle<cr> :desc :Explorer}
         {1 :<leader>fe
          2 :<cmd>NvimTreeFindFileToggle<cr>
-         :desc "Explorer Focus File"}]
+         :desc "Explorer Focus File"}
+        {1 :<leader>fE
+         2 "<cmd>NvimTreeToggle %:p:h<cr>"
+         :desc "Explorer Parent Directory"}
+        {1 :<leader>fp
+         2 "<cmd>NvimTreeToggle $PWD<cr>"
+         :desc "Explorer PWD"}
+         ]
  :cmd [:NvimTreeToggle :NvimTreeFocus :NvimTreeFindFile :NvimTreeCollapse]
  :opts {}
  :config #((. (require :nvim-tree) :setup) $2)}
--- lua/binhvim/plugins/tree.lua
+++ lua/binhvim/plugins/tree.lua
@@ -2,4 +2,4 @@
 local function _1_(_241, _242)
   return (require("nvim-tree")).setup(_242)
 end
-return {"nvim-tree/nvim-tree.lua", version = "*", keys = {{"<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Explorer"}, {"<leader>fe", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Explorer Focus File"}}, cmd = {"NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile", "NvimTreeCollapse"}, opts = {}, config = _1_}
+return {"nvim-tree/nvim-tree.lua", version = "*", keys = {{"<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Explorer"}, {"<leader>fe", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Explorer Focus File"}, {"<leader>fE", "<cmd>NvimTreeToggle %:p:h<cr>", desc = "Explorer Parent Directory"}, {"<leader>fp", "<cmd>NvimTreeToggle $PWD<cr>", desc = "Explorer PWD"}}, cmd = {"NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile", "NvimTreeCollapse"}, opts = {}, config = _1_}
